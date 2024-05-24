0cf9ea15ba4fb87f4a3498b47a5dd08b53564460
vinayakstha <vsth005@gmail.com>
2024-05-24 16:07:29 +0545

    manageBooks

--- ManageBooksPanel.java
+++ ManageBooksPanel.java
@@ -6,29 +6,32 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 class TextField extends JTextField {
-    TextField() {
+    TextField(int x, int y, int width, int height) {
+        this.setBounds(x, y, width, height);
         this.setFont(new Font("Arial", Font.PLAIN, 17));
         this.setVisible(true);
     }
 }
 
-class BookIdTextField extends TextField {
-    BookIdTextField() {
-        this.setBounds(50, 70, 350, 30);
-    }
-}
-
-class BookIdLabel extends JLabel {
-    BookIdLabel() {
-        this.setText("Book ID");
-        this.setBounds(50, 40, 200, 30);
+class Label extends JLabel {
+    Label(String text, int x, int y, int width, int height) {
+        this.setText(text);
+        this.setBounds(x, y, width, height);
         this.setFont(new Font("Arial", Font.PLAIN, 16));
     }
 }
 
 public class ManageBooksPanel extends JPanel {
-    BookIdLabel searchLabel = new BookIdLabel();
-    BookIdTextField bookIdTextField = new BookIdTextField();
+    // -----------------LABEL--------------------------
+    Label bookIdLabel = new Label("Book Id", 50, 40, 200, 30);
+    Label bookNameLabel = new Label("Name", 500, 40, 200, 30);
+    Label bookAuthorLabel = new Label("Author", 50, 120, 200, 30);
+    Label bookGenreLabel = new Label("Genre", 500, 120, 200, 30);
+    // ------------------TEXTFIELD--------------------------
+    TextField bookIdTextField = new TextField(50, 70, 350, 30);
+    TextField bookNameTextField = new TextField(500, 70, 350, 30);
+    TextField bookAuthorTextField = new TextField(50, 150, 350, 30);
+    TextField bookGenreTextField = new TextField(500, 150, 350, 30);
 
     ManageBooksPanel() {
         this.setBounds(200, 55, 950, 615);
@@ -37,8 +40,14 @@ public class ManageBooksPanel extends JPanel {
         img.setBounds(0, 0, 950, 615);
         this.setLayout(null);
         this.add(img);
-        img.add(searchLabel);
+        img.add(bookIdLabel);
         img.add(bookIdTextField);
+        img.add(bookNameTextField);
+        img.add(bookNameLabel);
+        img.add(bookAuthorLabel);
+        img.add(bookGenreLabel);
+        img.add(bookAuthorTextField);
+        img.add(bookGenreTextField);
         this.setVisible(false);
     }
 }
